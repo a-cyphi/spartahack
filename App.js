25cb8074c686b2a92132016d98563b61fb7b107d
arriolajp <arriolajp7@gmail.com>
2024-01-27 21:38:59 -0500

    logo + colored text

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Image,Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RoccoScreen from './rocco'; // Replace './Rocco' with the actual path to your Rocco.js file
@@ -10,12 +10,14 @@ const Stack = createStackNavigator();
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Tabbi!</Text>
+      <StatusBar style="auto"/>
+      <Image source={require('./images/tabbi.png')}/>
+      
       <Button
         title="Go to Rocco"
         onPress={() => navigation.navigate('Rocco')}
       />
-      <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -34,7 +36,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fbffb9',
     alignItems: 'center',
     justifyContent: 'center',
   },
